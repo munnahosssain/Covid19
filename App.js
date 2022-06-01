ab6129f211b0be0e3e6179c55aae35ae1217b58b
Munna Hossain <mayinuddin.munna@gmail.com>
2022-06-01 18:50:25 +0600

    enviroment setup

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Text from './src/Components/text/text';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import { View } from 'react-native'
+import React from 'react'
+import Text from '../text/text'
+
+export default function Header() {
+  return (
+    <View>
+      <Text>Header</Text>
+    </View>
+  )
+}
